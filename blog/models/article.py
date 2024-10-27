e7ea0ec026542104a91f37786f1d3e55d9377f40
KonulBayramova <konul.bairamovaa@gmail.com>
2024-10-27 18:36:32 +0400

    Article Model was created
    This model saves author information, content, information about creation and modification time, picture and categories of the article.

---
+++ blog/models/article.py
@@ -0,0 +1,26 @@
+from django.db import models
+from autoslug import AutoSlugField
+# from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from .category import CategoryModel
+User = get_user_model()
+
+class ArticleModel(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+                                  related_name="articles")
+    title = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from="title", unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # take the currect time
+    modified_at = models.DateTimeField(auto_now=True) # each time the modified time write to the database
+    published_at = models.DateTimeField(null=True, blank=True)
+    picture = models.ImageField(upload_to='article_images')
+    categories = models.ManyToManyField(CategoryModel)
+
+    class Meta:
+        db_table = 'article'
+        verbose_name = 'article'
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return f"{self.title}, {self.author.username}"
