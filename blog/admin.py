0ff82c73d7bd00dc439a1ef2a5bd363671e92eef
KonulBayramova <konul.bairamovaa@gmail.com>
2024-10-27 18:39:03 +0400

    Changes in the admin part

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CategoryModel
+from .models import CategoryModel, ArticleModel
 
 
 admin.site.register(CategoryModel)
