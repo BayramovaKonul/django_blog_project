9b40affa4dc8510cc2d392220243ed28f451c97c
KonulBayramova <konul.bairamovaa@gmail.com>
2024-11-06 21:59:57 +0400

    feat..TagModel was created
    TagModel was created with the fields of name, slug and created_at, articles and tags tables have manytomany connection.

--- blog/models/__init__.py
+++ blog/models/__init__.py
@@ -3,4 +3,5 @@ from .article import ArticleModel
 from .comment import CommentModel
 from .contact_us import ContactUsModel
 from .abstract_models import CreationDateAbstractModel
+from .tag import TagModel
 
